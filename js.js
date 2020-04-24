65b12144b26b9b7f62abfcedbbcd32c02d67a358
optimoi <antje_weckmann@yahoo.de>
2020-04-24 19:26:22 +0200

    Headerbereich fertig, Switchbar

---
+++ js.js
@@ -0,0 +1,65 @@
+window.onload = function(){
+
+var slideIndex = 0;
+
+showSlides();
+
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  for (i = 0; i < dots.length; i++) {
+   dots[i].className = dots[i].className.replace(" active", "");
+   dots[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 250000); // Change image every 2 seconds
+}
+
+
+
+  var switcher = document.getElementById("switcher");
+  var banner = document.getElementById("werbebanner-top");
+  var counter = 2;
+  switcher.onclick = function () {
+
+  var switchelements = document.getElementsByClassName("switch");
+
+
+  if (counter % 2 !== 1) {
+    hideSlider();
+    banner.style.display = "block"
+    counter++;
+  }
+    else {
+      showSlider();
+      counter++;
+      banner.style.display = "none"
+
+    }
+function hideSlider() {
+    for (i = 0; i < switchelements.length; i++) {
+      switchelements[i].style.display = "none";
+
+    }
+
+  }
+
+function showSlider() {
+    for (i = 0; i < switchelements.length; i++) {
+        switchelements[i].style.display = "block";
+
+      }
+
+    }
+  }
+}
