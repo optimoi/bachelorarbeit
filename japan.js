2acff4570eae02377e8d820e793d3571264cd8c3
optimoi <antje_weckmann@yahoo.de>
2020-05-03 14:14:40 +0200

    Colc 1 JPN 1

--- japan.js
+++ japan.js
@@ -2,6 +2,8 @@ window.onload = function(){
 
   var switcher = document.getElementById("switcher");
   var topbanner = document.getElementById("volle-breite-grau");
+  var navi = document.getElementById("volle-breite-blau");
+  var navcolor = document.getElementsByClassName("nav-a-color");
   var platzhalter = document.getElementById("platzhalter");
   var counter = 2;
 
@@ -9,12 +11,18 @@ window.onload = function(){
 
   if (counter % 2 !== 1) {
     topbanner.style.display = "none";
-    platzhalter.style.display = "block";
+    navi.style.backgroundColor = "#efefef";
+    for (i = 0; i < navcolor.length; i++) {
+      navcolor[i].style.color = "#121212";
+    }
     counter++;
   }
     else {
-      topbanner.style.display = "block"
-      platzhalter.style.display = "none"
+      topbanner.style.display = "block";
+      navi.style.backgroundColor = "#003E70";
+      for (i = 0; i < navcolor.length; i++) {
+        navcolor[i].style.color = "#FFFFFF";
+      }
       counter++;
     }
   }
