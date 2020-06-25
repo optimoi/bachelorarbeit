46fd78efbb3048fda43e16b76c612687c5c8c7fc
optimoi <antje_weckmann@yahoo.de>
2020-06-25 14:09:04 +0200

    KORRS / FEINHEITEN FAST FIN

--- javascript/china.js
+++ javascript/china.js
@@ -24,7 +24,7 @@ function showSlides() {
   }
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 7000); // Change image every 2 seconds
+  setTimeout(showSlides, 3000); // Change image every 2 seconds
 }
 
 function showSlides1() {
